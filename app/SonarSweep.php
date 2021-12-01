134ac051b81ce5fd37db6883d247fb36dcd237ad
Steve Adams <irwtdvoys@gmail.com>
2021-12-01 16:42:45 +0000

    Initial project setup

---
+++ app/SonarSweep.php
@@ -0,0 +1,51 @@
+<?php
+	namespace App;
+
+	use AoC\Helper;
+	use AoC\Result;
+
+	class SonarSweep extends Helper
+	{
+		private array $data;
+
+		public function __construct(int $day, string $override = null)
+		{
+			parent::__construct($day);
+
+			$raw = parent::load($override);
+			$rows = explode(PHP_EOL, $raw);
+
+			$this->data = array_map("intval", $rows);
+		}
+
+		private function process(int $size): int
+		{
+			$count = 0;
+
+			for ($index = 0; $index < count($this->data) - ($size - 1); $index++)
+			{
+				$window = array_slice($this->data, $index, $size);
+				$sum = array_sum($window);
+
+				if (isset($current) && $sum > $current)
+				{
+					$count++;
+				}
+
+				$current = $sum;
+			}
+
+			return $count;
+		}
+
+		public function run(): Result
+		{
+			$result = new Result(
+				$this->process(1),
+				$this->process(3)
+			);
+
+			return $result;
+		}
+	}
+?>
