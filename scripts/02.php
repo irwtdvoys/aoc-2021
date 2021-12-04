fe1e31aeb3d45a027dc457ace6ee1cb251755227
Steve Adams <irwtdvoys@gmail.com>
2021-12-04 19:17:07 +0000

    Add results to day 1 + 2

--- scripts/02.php
+++ scripts/02.php
@@ -5,6 +5,9 @@
 
 	use App\Dive;
 
-	$helper = new Dive(2, ROOT . "data/02/example");
+	$helper = new Dive(2);//, ROOT . "data/02/example");
 	$helper->run()->output();
+
+	// Part 1: 2019945
+	// Part 2: 1599311480
 ?>
